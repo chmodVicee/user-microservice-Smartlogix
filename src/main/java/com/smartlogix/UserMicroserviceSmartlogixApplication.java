3ed6850ba079c6895f29fc14ec20f624baa1b588
vicente <vicealvarezzz@gmail.com>
2026-06-01 18:06:31 -0400

    first commit

---
+++ src/main/java/com/smartlogix/UserMicroserviceSmartlogixApplication.java
@@ -0,0 +1,13 @@
+package com.smartlogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroserviceSmartlogixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMicroserviceSmartlogixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartlogix/UserMicroserviceSmartlogixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceSmartlogixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
