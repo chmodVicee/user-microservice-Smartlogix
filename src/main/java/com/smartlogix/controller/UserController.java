cbba125291b41ed02a11a2e2bbcd4ccfeb94842e
vicente <vicealvarezzz@gmail.com>
2026-06-04 13:36:35 -0400

    feat(controller): Creación de UserController y AuthController

--- src/main/java/com/smartlogix/controller/UserController.java
+++ src/main/java/com/smartlogix/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.smartlogix.controller;
 
+import com.smartlogix.dto.UserResponse;
+import com.smartlogix.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserResponse> getProfile() {
+        return ResponseEntity.ok(userService.getProfile());
+    }
+
+    //solo el usuario ADMIN puede listar a todos los usuarios
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
 }
