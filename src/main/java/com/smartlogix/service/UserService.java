64145764ea2b73c6777f1f0f31dd4c12c526edb7
vicente <vicealvarezzz@gmail.com>
2026-06-04 13:29:53 -0400

    feat(service): Creación de UserService

--- src/main/java/com/smartlogix/service/UserService.java
+++ src/main/java/com/smartlogix/service/UserService.java
@@ -1,4 +1,43 @@
 package com.smartlogix.service;
 
+import com.smartlogix.dto.UserResponse;
+import com.smartlogix.model.User;
+import com.smartlogix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    private UserResponse mapToResponse(User user) {
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole()
+        );
+    }
+
+    public UserResponse getProfile(){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("Usuario no encontrado: "+username));
+        return mapToResponse(user);
+    }
+
+    public List<UserResponse> getAllUsers(){
+        return userRepository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
 }
