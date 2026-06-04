2a5455b12e5bd602693c04b2cf2b95dcda2f2a01
AgusBalles <ag.ballesteros@duocuc.cl>
2026-06-04 16:51:33 -0400

    se corrijio typo en existsByUsername y existsByEmail en UserRepository

--- src/main/java/com/smartlogix/repository/UserRepository.java
+++ src/main/java/com/smartlogix/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
    Optional<User> findByUsername(String username);
-   boolean existByUsername(String username);
-   boolean existByEmail(String email);
+   boolean existsByUsername(String username);
+   boolean existsByEmail(String email);
 }
 
