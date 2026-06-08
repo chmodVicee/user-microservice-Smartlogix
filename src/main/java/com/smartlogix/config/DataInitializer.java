c30b999387bc4327a05324e7a4152450f6de5ed7
vicente <vicealvarezzz@gmail.com>
2026-06-08 11:25:31 -0400

    Utilizar credenciales de ADMIN para la base de datos

--- src/main/java/com/smartlogix/config/DataInitializer.java
+++ src/main/java/com/smartlogix/config/DataInitializer.java
@@ -1,4 +1,44 @@
 package com.smartlogix.config;
 
-public class DataInitializer {
+import com.smartlogix.model.Role;
+import com.smartlogix.model.User;
+import com.smartlogix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (!userRepository.existsByUsername(adminUsername)) {
+            User admin = User.builder()
+                    .username(adminUsername)
+                    .email(adminEmail)
+                    .password(passwordEncoder.encode(adminPassword))
+                    .role(Role.ADMIN)
+                    .build();
+            userRepository.save(admin);
+            System.out.println(">>> Admin creado");
+        } else {
+            System.out.println(">>> Admin ya existe");
+        }
+    }
 }
